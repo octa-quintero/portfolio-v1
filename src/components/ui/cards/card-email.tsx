cddcea77078e1720d5090815d7b6728b9a8db1d9
octa-quintero <octa.quinteroo@gmail.com>
2025-01-21 14:08:17 +0100

    changes lenguaje

--- src/components/ui/cards/card-email.tsx
+++ src/components/ui/cards/card-email.tsx
@@ -24,8 +24,7 @@ export default function CardEmail({
       href={`mailto:${link}`}
       target="_blank"
       className={`group bg-gradient-to-t from-[#1e2124] to-[#2f3136] grid place-items-center outline-slate-50 relative rounded-2xl transition-all duration-300 w-full z-50 col-start-${colStart} col-span-${colSpan} row-start-${rowStart} row-span-${rowSpan} overflow-hidden`}
-    >
-      {/* Fondo con transición en hover */}
+>
       <span className="absolute inset-0 bg-[#f1f3f4] opacity-0 transition-all duration-500 rounded-full scale-0 group-hover:scale-150 group-hover:opacity-80" />
 
       {/* Icono blanco de Gmail */}
