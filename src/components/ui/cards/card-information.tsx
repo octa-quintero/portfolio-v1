668efe4158c64a4d779175526f387745c0ae5ded
octa-quintero <octa.quinteroo@gmail.com>
2025-02-09 21:29:45 +0100

    border animated

--- src/components/ui/cards/card-information.tsx
+++ src/components/ui/cards/card-information.tsx
@@ -3,7 +3,6 @@
 import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 
-
 interface CardInformationInterface {
   image: string;
   title: string;
@@ -36,9 +35,10 @@ export default function CardInformation({
       initial={{ opacity: 0, x: -100 }}
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 1 }}
-      className={`${background} ${gridClasses} flex outline-slate-50 relative rounded-2xl transition w-full h-full z-50 p-10 `} // Padding agregado aquí
+      className={`relative ${background} ${gridClasses} flex rounded-2xl transition w-full h-full z-50 p-10`}
     >
-      {/* Fondo GIF transparente */}
+      <div className="border-gradient-animation absolute inset-0 rounded-xl"></div>
+      
       <div
         className="absolute inset-0 z-0 rounded-2xl overflow-hidden"
         style={{
@@ -46,28 +46,12 @@ export default function CardInformation({
           opacity: 0.08,
         }}
       ></div>
-
-<div
-  className="flex flex-col justify-end items-start w-1/2 h-full p-0 z-10 rounded-l-2xl"
-  style={{ fontFamily: 'MyFont' }}
->
-  <p className="text-left text-3xl text-white font-italic m-0">
-    {name ? t(name) : ''}
-  </p>
-
-  {/* Aplicando 'MyFont1' a este párrafo */}
-  <p
-    className="text-left text-5xl text-white font-semibold m-0"
-    style={{ fontFamily: 'MyFont' }}
-  >
-    {t(title)}
-  </p>
-
-  {/* Aplicando la fuente 'Quicksand' */}
-  <p className={`font-quicksand font-bold text-xl text-white`} style={{ fontFamily: 'MyFont1' }}>
-    {t(text)}
-  </p>
-</div>
+      
+      <div className="flex flex-col justify-end items-start w-1/2 h-full p-0 z-10 rounded-l-2xl" style={{ fontFamily: 'MyFont' }}>
+        <p className="text-left text-3xl text-white font-italic m-0">{name ? t(name) : ''}</p>
+        <p className="text-left text-5xl text-white font-semibold m-0" style={{ fontFamily: 'MyFont' }}>{t(title)}</p>
+        <p className="font-quicksand font-bold text-xl text-white" style={{ fontFamily: 'MyFont1' }}>{t(text)}</p>
+      </div>
 
       <div
         className="flex flex-col justify-start items-start w-1/2 h-full p-0 z-10 rounded-l-2xl"
