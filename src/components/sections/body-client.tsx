975f1247ccf4636da8e08a627e7f2cb64223c965
octa-quintero <octa.quinteroo@gmail.com>
2025-03-19 22:24:43 +0100

    responsive

--- src/components/sections/body-client.tsx
+++ src/components/sections/body-client.tsx
@@ -9,12 +9,16 @@ export default function BodyClient({ children }: Readonly<{
   const { darkMode } = useStore();
 
   return (
-<body
-className={`${darkMode ? "" : "bg-[#171717]"} min-h-screen w-full transition-all duration-1000 ease-in-out pt-10 px-40 pb-10`}
-
->
-  {children}
-</body>
-
+    <body
+      className={`${darkMode ? "" : "bg-[#171717]"} min-h-screen w-full transition-all duration-1000 ease-in-out pt-10 px-4 sm:px-10 md:px-20 lg:px-40 pb-10`}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: 'auto',
+      }}
+    >
+      {children}
+    </body>
   );
 }
